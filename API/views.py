6793897a94a1e6839922a3d767bc35e74c247dab
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-08-08 14:22:23 +0300

    création d'une vue

--- API/views.py
+++ API/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import UploadedImage
+from .serializers import UploadedImageSerializer
+
+class UploadedImageViewSet(viewsets.ModelViewSet):
+    queryset = UploadedImage.objects.all()
+    serializer_class = UploadedImageSerializer
